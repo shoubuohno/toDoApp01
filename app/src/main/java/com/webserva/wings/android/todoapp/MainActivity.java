c51dfd91207f5b475851e179dce5356ffbf7ce0f
ohnoshobu <ohno56sent@gmail.com>
2022-05-22 15:42:41 +0900

    入力した項目がリストに追加される機能実装

--- app/src/main/java/com/webserva/wings/android/todoapp/MainActivity.java
+++ app/src/main/java/com/webserva/wings/android/todoapp/MainActivity.java
@@ -3,6 +3,14 @@ package com.webserva.wings.android.todoapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //ListViewオブジェクトの取得
+        ListView lvPurpose = findViewById(R.id.lvPurpose);
+        List<String> purposeList = new ArrayList<>();
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, purposeList);
+        ///設定ボタンの取得
+        Button inputBtn = findViewById(R.id.btCreate);
+        inputBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //要素追加
+                EditText purpose = findViewById(R.id.etPurposeForm);
+                //EditText(テキスト)を取得し、アダプタに追加
+                purposeList.add(purpose.getText().toString());
+            }
+        });
+        lvPurpose.setAdapter(adapter);
     }
+
 }
